42b03f1469d4103cef08c467f98e410fba2de395
George Nascimento <geo.nascimento.job@gmail.com>
2022-08-15 10:18:37 -0300

    Mini projeto para estudo de orientação a objetos em C#

---
+++ ExercicioFinal/ContaBancaria.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioFinal
+{
+    internal class ContaBancaria
+    {
+        /*Atributos com auto properties*/
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } //o private set impede a mudança direta, mas permite a mudança via métodos de classe//
+
+        /*construtures*/
+        public ContaBancaria (int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            /*Saldo = saldo; essa atribuição é possivel, mas seria melhor usar o método deposito, pois ele comtem a logica de deposito*/
+            Deposito(depositoInicial);
+        }
+        
+        /*Métodos de classe*/
+        public void Deposito(double quantia)
+        {
+            Saldo = Saldo + quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            double taxa = 5.0;
+            Saldo = Saldo - (quantia + taxa);
+        }
+
+
+        /*Método To.String()*/
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ExercicioFinal/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioFinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Declarar o objeto*/
+            ContaBancaria conta; /*Declarei fora para não dar problema de escopo com a estutura if else*/
+
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.Write("Haverá deposito inicial (s/n): ");
+            char resposta = char.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.WriteLine("Entre com o valor do depósito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                conta = new ContaBancaria(numero, titular, depositoInicial); /*Instanciar o objeto com o construtor personalizado*/
+            }
+            else
+            {
+                conta = new ContaBancaria(numero, titular); /*Instanciar o objeto com o construtor personalizado*/
+            }
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com o valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);   
+            Console.WriteLine("Dados atualizados:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre com um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine(conta);
+
+                   
+        }
+    }
+}
